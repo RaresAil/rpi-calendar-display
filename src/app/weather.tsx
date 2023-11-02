dfea4977a3d75db362a0ae17d94e9d5fab3ed237
RaresAil <ailincairares1@gmail.com>
2023-11-02 17:29:05 +0200

    feat/weather-data

--- src/app/weather.tsx
+++ src/app/weather.tsx
@@ -3,72 +3,6 @@ import "./weather.css";
 
 const Icons = require("@intern0t/react-weather-icons");
 
-const mock = {
-  latitude: 47.1875,
-  longitude: 27.625,
-  generationtime_ms: 0.06008148193359375,
-  utc_offset_seconds: 0,
-  timezone: "GMT",
-  timezone_abbreviation: "GMT",
-  elevation: 72.0,
-  current_units: {
-    time: "unixtime",
-    interval: "seconds",
-    temperature_2m: "°C",
-    relativehumidity_2m: "%",
-    weathercode: "wmo code",
-  },
-  current: {
-    time: 1698925500,
-    interval: 900,
-    temperature_2m: 13.4,
-    relativehumidity_2m: 72,
-    weathercode: 3,
-  },
-  hourly_units: {
-    time: "unixtime",
-    temperature_2m: "°C",
-    relativehumidity_2m: "%",
-    weathercode: "wmo code",
-  },
-  hourly: {
-    time: [
-      1698883200, 1698886800, 1698890400, 1698894000, 1698897600, 1698901200,
-      1698904800, 1698908400, 1698912000, 1698915600, 1698919200, 1698922800,
-      1698926400, 1698930000, 1698933600, 1698937200, 1698940800, 1698944400,
-      1698948000, 1698951600, 1698955200, 1698958800, 1698962400, 1698966000,
-      1698969600, 1698973200, 1698976800, 1698980400, 1698984000, 1698987600,
-      1698991200, 1698994800, 1698998400, 1699002000, 1699005600, 1699009200,
-      1699012800, 1699016400, 1699020000, 1699023600, 1699027200, 1699030800,
-      1699034400, 1699038000, 1699041600, 1699045200, 1699048800, 1699052400,
-      1699056000, 1699059600, 1699063200, 1699066800, 1699070400, 1699074000,
-      1699077600, 1699081200, 1699084800, 1699088400, 1699092000, 1699095600,
-      1699099200, 1699102800, 1699106400, 1699110000, 1699113600, 1699117200,
-      1699120800, 1699124400, 1699128000, 1699131600, 1699135200, 1699138800,
-    ],
-    temperature_2m: [
-      7.8, 7.3, 7.2, 6.6, 6.3, 6.3, 6.6, 8.4, 9.9, 11.2, 12.7, 13.3, 13.5, 13.3,
-      12.7, 11.5, 10.6, 10.1, 9.7, 9.3, 9.1, 9.2, 9.7, 10.5, 10.8, 10.7, 11.0,
-      11.3, 11.4, 11.5, 11.9, 13.1, 14.5, 15.7, 16.5, 17.5, 18.1, 18.3, 17.8,
-      16.7, 16.2, 15.8, 15.6, 15.5, 15.4, 15.6, 15.4, 15.4, 15.3, 15.4, 15.5,
-      15.7, 15.9, 15.8, 15.9, 16.4, 17.3, 17.7, 17.9, 18.6, 19.1, 19.4, 19.0,
-      18.3, 17.8, 17.5, 16.8, 17.6, 17.5, 17.4, 17.0, 16.5,
-    ],
-    relativehumidity_2m: [
-      93, 92, 89, 90, 91, 90, 91, 86, 83, 90, 79, 75, 71, 69, 75, 82, 86, 89,
-      89, 89, 90, 90, 92, 91, 91, 93, 92, 91, 91, 92, 91, 88, 83, 77, 72, 68,
-      65, 64, 67, 73, 72, 77, 82, 86, 88, 88, 89, 90, 92, 94, 95, 95, 95, 95,
-      95, 93, 88, 85, 84, 79, 76, 77, 81, 83, 83, 87, 96, 91, 88, 85, 85, 85,
-    ],
-    weathercode: [
-      0, 0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 1, 1, 1, 2, 3, 3, 3,
-      2, 2, 2, 2, 3, 2, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 3, 3, 3, 3, 61, 61, 61,
-      51, 51, 3, 3, 3, 3, 3, 3, 3, 80, 2, 3, 3, 3, 80, 3, 80, 63, 61, 3, 3, 3,
-      3,
-    ],
-  },
-};
-
 export class Weather extends React.PureComponent<{}, State> {
   private readonly UNIT_TEMP = "°C";
 
@@ -263,7 +197,15 @@ export class Weather extends React.PureComponent<{}, State> {
   };
 
   private loadWeatherData = async () => {
-    this.parseData(mock);
+    const result = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${process.env.REACT_APP_LAT}&longitude=${process.env.REACT_APP_LONG}&current=temperature_2m,relativehumidity_2m,weathercode&hourly=temperature_2m,relativehumidity_2m,weathercode&timeformat=unixtime&timezone=GMT&forecast_days=3`,
+      {
+        method: "GET",
+      }
+    );
+
+    const data = await result.json();
+    this.parseData(data);
   };
 
   private updateHourlyData = async () => {

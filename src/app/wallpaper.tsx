4ec81d90e213e6e2672dd4fc66cbf972bf2c0440
RaresAil <ailincairares1@gmail.com>
2023-11-02 22:58:35 +0200

    remove-wallpaper

--- src/app/wallpaper.tsx
+++ src/app/wallpaper.tsx
@@ -1,34 +1,8 @@
 import React from "react";
 import "./wallpaper.css";
 
-export class Wallpaper extends React.PureComponent<{}, { w: number }> {
-  private readonly numWallpapers = 6;
-  private interval: NodeJS.Timeout | undefined;
-
-  state = {
-    w: 1,
-  };
-
-  componentDidMount(): void {
-    this.interval = setInterval(() => {
-      if (this.state.w === this.numWallpapers) {
-        this.setState({ w: 1 });
-      } else {
-        this.setState({
-          w: this.state.w + 1,
-        });
-      }
-    }, 60 * 1000);
-  }
-
-  componentWillUnmount(): void {
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.interval = undefined;
-    }
-  }
-
+export class Wallpaper extends React.PureComponent {
   render(): React.ReactNode {
-    return <div className={`wallpaper bg${this.state.w}`} />;
+    return <div className="wallpaper" />;
   }
 }
